8598a2653038949b080e72a482bf1e123a4c3804
anwesha-09 <maityanwesha26@gmail.com>
2020-04-27 01:46:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/apkproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.apkproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void SEND (View view{
+        Toast.makeText
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
